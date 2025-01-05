1b8b68bf1c402a70a3fbd437e388f66d0ed8a870
Yoki Jati Perkasa <yokijati@gmail.com>
2025-01-05 20:37:51 +0700

    Success connect Tailwind CSS and Vue JS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+    <p class="text-teal-300">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
